b579756dfc38634deb33dbfac938acfc5582246c
marina-stack <80834133+marina-stack@users.noreply.github.com>
2021-10-15 13:35:32 +0300

    Update Rock Papper Scissors Game.kt

--- Rock Papper Scissors Game.kt
+++ Rock Papper Scissors Game.kt
@@ -3,4 +3,15 @@ fun main (args:Array<String>) {
     val gameResult = getGameChoise(events)
 }
 
+//computer chooses option
 fun getGameChoise (eventsParam: Array<String>) = eventsParam [(Math.random()*eventsParam.size).toInt()]
+
+fun getUserChoice (eventsParam: Array<String>): String {
+    //an option request from the user
+    print ("Пожалуйста, выберите одно из следующих значений:")
+    for (index in eventsParam) print (" $index")
+    print (".")
+    //read user input
+    val userInput = readLine ()
+
+}
